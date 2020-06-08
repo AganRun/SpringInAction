3a7408074213997838708b91752d1e825a2eccd6
AganRun <1379154067@qq.com>
2020-06-08 21:34:37 +0800

    feat(fix) 。

--- Chapter2/taco-web/src/main/java/com/agan/tacocloud/controller/OrderController.java
+++ Chapter2/taco-web/src/main/java/com/agan/tacocloud/controller/OrderController.java
@@ -1,6 +1,6 @@
 package com.agan.tacocloud.controller;
 
-import com.agan.tacocloud.po.Order;
+import com.agan.tacocloud.pojo.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
