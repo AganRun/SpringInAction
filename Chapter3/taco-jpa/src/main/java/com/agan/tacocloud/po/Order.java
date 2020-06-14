27481526fc3ebc559dcc5810988a1d77ba544735
AganRun <1379154067@qq.com>
2020-06-14 17:54:15 +0800

    feat(jpa) 运行项目

--- Chapter3/taco-jpa/src/main/java/com/agan/tacocloud/po/Order.java
+++ Chapter3/taco-jpa/src/main/java/com/agan/tacocloud/po/Order.java
@@ -25,31 +25,31 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column
+    
     private Date placedAt;
-    @Column
+    
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
-    @Column
+    
     @NotBlank(message = "Street is required")
     private String deliveryStreet;
-    @Column
+    
     @NotBlank(message = "City is required")
     private String deliveryCity;
-    @Column
+    
     @NotBlank(message = "State is required")
     private String deliveryState;
-    @Column
+    
     @NotBlank(message = "Zip code is required")
     private String deliveryZip;
-    @Column
+    
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
-    @Column
+    
     @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
             message = "Must be formatted MM/YY")
     private String ccExpiration;
-    @Column
+    
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
