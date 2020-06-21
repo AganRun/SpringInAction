55d3ba91407ea24cb4f68127ae4adcfca970e797
AganRun <1379154067@qq.com>
2020-06-21 18:31:55 +0800

    feat(configure) 自定义配置属性

--- Chapter5/taco-config/src/main/java/com/agan/tacocloud/controller/OrderController.java
+++ Chapter5/taco-config/src/main/java/com/agan/tacocloud/controller/OrderController.java
@@ -4,8 +4,14 @@ import javax.validation.Valid;
 
 import com.agan.tacocloud.dao.OrderRepository;
 import com.agan.tacocloud.po.Order;
+import com.agan.tacocloud.po.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +22,16 @@ import org.springframework.web.bind.support.SessionStatus;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
 
     private OrderRepository orderRepo;
+
+    private int pageSize = 20;
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
 
     @Autowired
     public OrderController(OrderRepository orderRepo) {
@@ -40,4 +53,11 @@ public class OrderController {
         return "redirect:/";
     }
 
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+//        Pageable pageable = PageRequest.of(0, 20);
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
+    }
 }
--- Chapter5/taco-config/src/main/java/com/agan/tacocloud/dao/OrderRepository.java
+++ Chapter5/taco-config/src/main/java/com/agan/tacocloud/dao/OrderRepository.java
@@ -2,10 +2,15 @@ package com.agan.tacocloud.dao;
 
 
 import com.agan.tacocloud.po.Order;
+import com.agan.tacocloud.po.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
